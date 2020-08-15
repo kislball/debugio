f6b19c1614c8eff812ea88e473ff43b5f318e251
Кислый <kislball@yahoo.com>
2020-08-15 23:07:42 +0300

    some packahe.json publishing fixes

--- types/src/index.d.ts
+++ types/src/index.d.ts
@@ -11,25 +11,78 @@ declare interface DebugIO {
     emit<U extends keyof IDebugIOEvents>(event: U, ...args: Parameters<IDebugIOEvents[U]>): boolean;
 }
 declare class DebugIO extends EventEmitter {
+    /**
+     * options
+     */
     readonly options: IOptions;
+    /**
+     * namespace
+     */
     namespace: string;
+    /**
+     * children of the instance
+     */
     children: DebugIO[];
+    /**
+     * parents of the instance
+     */
     parents: DebugIO[];
+    /**
+     * recivers of the instance
+     */
     recivers: LogReciver[];
+    /**
+     * prefix variable in main placeholder
+     */
     static prefix: string;
+    /**
+     * placeholders of the instance
+     */
     static placeholders: IPlaceholders;
+    /**
+     * separator which messages are joined with
+     */
     static separator: string;
+    /**
+     * render functions
+     */
     static render: TemplateEngine;
+    /**
+     * standart recievers
+     */
     static recivers: {
         Console: (logType: LogType, message: any, messages: any[], pretty: string) => void;
     };
     private static _createdNamespaces;
+    /**
+     *
+     * @param options - options
+     */
     constructor(options: IOptions);
+    /**
+     * log data
+     *
+     * @param messages data to log
+     */
     log(...messages: any[]): void;
+    /**
+     * output error
+     *
+     * @param messages errors
+     */
     error(...messages: any[]): void;
+    /**
+     * output a warning
+     *
+     * @param messages warn
+     */
     warn(...messages: any[]): void;
+    /**
+     * use a reciver for the instance
+     *
+     * @param reciver reciver to use
+     */
     use(reciver: LogReciver): void;
     private _pretty;
-    addChild(child: DebugIO): void;
 }
 export = DebugIO;
--- types/src/template.d.ts
+++ types/src/template.d.ts
@@ -1,3 +1,8 @@
 import TemplateEngine = require('./types/TemplateEngine');
+/**
+ *
+ * @param str - string to render
+ * @param context - context to render
+ */
 declare const template: TemplateEngine;
 export = template;
