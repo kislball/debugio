08121efe581fb3c98dab5fcaac31bc5f02ce5217
Кислый <kislball@yahoo.com>
2020-08-16 11:02:35 +0300

    documenting options

--- src/types/IOptions.ts
+++ src/types/IOptions.ts
@@ -2,8 +2,20 @@ import DebugIO from '../index'
 import LogReciver = require("./LogReciver");
 
 export interface IOptions {
+  /**
+   * set parent instance
+   */
   parent?: DebugIO,
+  /**
+   * namespace
+   */
   namespace: string,
+  /**
+   * recivers to pass 
+   */
   recivers?: LogReciver[],
+  /**
+   * invoke parent recivers
+   */
   invokeParentRecivers?: boolean
 }
